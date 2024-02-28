7beb4c759509601ef33facba5f77623d49d564b2
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2024-02-28 15:56:45 -0300

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt">
+      <body className={inter.className}>
+        <h1 className="text-3xl">Nav Bar</h1>
+      {children}</body>
     </html>
   );
 }
